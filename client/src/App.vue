6d7e97c080280ae8be132aff1a9a8b691af5a443
wey <45823398+WeyTing@users.noreply.github.com>
2025-06-01 12:09:01 +0800

    [chores]加入說明

--- client/src/App.vue
+++ client/src/App.vue
@@ -5,6 +5,8 @@ import { auth } from "./config/firebase";
 import { useAuthStore } from "./stores/useAuthStore";
 
 // 初始化 user 狀態與 token（首次載入或切換帳號時）
+//近期使用時發現bug 一小時後的 token 會失效，導致無法正常使用 儲存在pinia的就會有bug
+// 這裡會在使用者登入或登出時自動更新狀態 目前還在測試階段 如果回去開發使用有碰到任何問題再提出來!! 
 onAuthStateChanged(auth, async (firebaseUser) => {
   const authStore = useAuthStore();
 
