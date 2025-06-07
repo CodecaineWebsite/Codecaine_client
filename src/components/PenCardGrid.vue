5062e67976ecf340fa45180a2d39fee5f353f1af
Vivi Sun <sun20317@gmail.com>
2025-06-07 18:44:15 +0800

    [feat] 新增PenCardGrid.vue 元件

---
+++ src/components/PenCardGrid.vue
@@ -0,0 +1,27 @@
+<template>
+  <div
+    class="grid [grid-template-columns:repeat(auto-fill,minmax(30%,1fr))] gap-12"
+  >
+    <penCard
+      v-for="card in pens"
+      :key="card.id"
+      :title="card.title"
+      :description="card.description"
+      :userName="card.username"
+      :userDisplayName="card.user_display_name"
+      :favorites_count="card.favorites_count"
+      :comments_count="card.comments_count"
+      :views_count="card.views_count"
+    />
+  </div>
+</template>
+
+<script setup>
+import penCard from "./penCard.vue";
+defineProps({
+  pens: {
+    type: Array,
+    required: true,
+  },
+});
+</script>
