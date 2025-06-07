4ad8cff019417f5f5cdb6b217848a90a72180fc5
Vivi Sun <sun20317@gmail.com>
2025-06-07 19:28:01 +0800

    [refactor] 將 PenCard.vue 的 props 改為接收物件, PenCardGrid 改名為 PenCardLayout

---
+++ src/components/PenCardLayout.vue
@@ -0,0 +1,21 @@
+<template>
+  <!-- grid layout -->
+  <div
+    class="grid [grid-template-columns:repeat(auto-fill,minmax(30%,1fr))] gap-12"
+  >
+    <penCard
+      v-for = "card in pens"
+      :pen = "card"
+    />
+  </div>
+</template>
+
+<script setup>
+import penCard from "./penCard.vue";
+defineProps({
+  pens: {
+    type: Array,
+    required: true,
+  },
+});
+</script>
