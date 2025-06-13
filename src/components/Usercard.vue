80529bfed8385c07898de47c31c5003e9f24013d
wey <qaz237562@gmail.com>
2025-06-13 12:18:45 +0800

    [feat]新增用戶卡片組件，顯示用戶名稱和頭像

---
+++ src/components/Usercard.vue
@@ -0,0 +1,34 @@
+<template>
+  <a href="">
+    <div
+      class="flex flex-row items-center gap-1 bg-gray-700 h-[60px] rounded-md group hover:text-white p-1"
+    >
+      <div class="w-12 h-12 overflow-hidden p-1">
+        <img src="/default-avatar.png" class="w-full object-cover rounded-md" />
+      </div>
+      <div class="overflow-hidden">
+        <div class="text-xl truncate max-w-30">
+          {{ props.displayName ? props.displayName : props.username }}
+        </div>
+        <div class="text-blue-300 text-xs group-hover:text-white">
+          @{{ props.username }}
+        </div>
+      </div>
+    </div>
+  </a>
+</template>
+
+<script setup>
+const props = defineProps({
+  username: {
+    type: String,
+    required: true,
+  },
+  displayName: {
+    type: String,
+  },
+  profileImage: {
+    type: null,
+  },
+});
+</script>
