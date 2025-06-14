925faf40a47c3fe7e8c57d2936a6509ab73a5899
chinyuting <s86052915@gmail.com>
2025-06-15 01:24:06 +0800

    [chore] add semicolons

--- src/views/Pen.vue
+++ src/views/Pen.vue
@@ -20,7 +20,7 @@
 
   const route = useRoute();
   const router = useRouter();
-  const workStore = useWorkStore()
+  const workStore = useWorkStore();
   const { updateCurrentCode, handleCurrentIdChange, updatePreviewSrc, moveToTrash }= workStore; //放function
   const { currentWork, currentId } = storeToRefs(workStore); //放資料
   handleCurrentIdChange(route.params.id)
