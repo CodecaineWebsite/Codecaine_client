e87e10215e2ffc37661e0ec1c52527d9613d0471
chinyuting <s86052915@gmail.com>
2025-06-15 01:49:42 +0800

    [refactor] use onMounted to fetch data

--- src/views/Pen.vue
+++ src/views/Pen.vue
@@ -23,7 +23,9 @@
   const workStore = useWorkStore();
   const { updateCurrentCode, handleCurrentIdChange, updatePreviewSrc, moveToTrash }= workStore; //放function
   const { currentWork, currentId } = storeToRefs(workStore); //放資料
-  handleCurrentIdChange(route.params.id)
+  onMounted( () => {
+    handleCurrentIdChange(route.params.id)
+  })
 
   const penHeader = ref(null)
   const htmlCode = ref('');
