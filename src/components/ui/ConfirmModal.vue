293aca133f80f14867e394be2d095696102fd517
Vivi <sun20317@gmail.com>
2025-06-19 15:06:24 +0800

    [feat] 將ConfirmModa.vue 導入至 DeletedPenCard做永久刪除確認

--- src/components/ui/ConfirmModal.vue
+++ src/components/ui/ConfirmModal.vue
@@ -1,34 +1,29 @@
 <template>
   <div class="fixed inset-0 bg-black/50 flex items-center justify-center z-50">
     <div
-    class="p-6 rounded-md max-w-sm w-full border-8"
-    :class="styleClasses.container"
+      class="p-6 rounded-md max-w-sm w-full border-8"
+      :class="styleClasses.container"
     >
-
-     <p class="font-bold mb-2 text-lg" v-if="$slots.title">
-      <slot name="title" />
-     </p>
-
+      <p class="font-bold mb-2 text-lg" v-if="$slots.title">
+        <slot name="title" />
+      </p>
 
       <div class="text-xs text-cc-1 mb-4">
         <slot name="message" />
       </div>
 
-       <div class="flex text-sm justtify-start space-x-2">
-         <button
+      <div class="flex text-sm justify-start space-x-2">
+        <button
           :disabled="confirming"
           @click="onConfirm"
           :class="styleClasses.confirm"
-         >
-         {{ confirming ? loadingText : confirmText }}
-         </button>
-         <button
-         @click="onCancel"
-         class="bg-cc-13 px-4.5 py-2.5 rounded"
-         >
-         {{ cancleText }}
-         </button>
-       </div>
+        >
+          {{ confirming ? loadingText : confirmText }}
+        </button>
+        <button @click="onCancel" class="bg-cc-13 px-4.5 py-2.5 rounded">
+          {{ cancelText }}
+        </button>
+      </div>
     </div>
   </div>
 </template>
@@ -38,31 +33,31 @@ import { computed } from "vue";
 const props = defineProps({
   confirmText: {
     type: String,
-    default: "Confirm"
+    default: "Confirm",
   },
   cancelText: {
     type: String,
-    default: "Cancel"
+    default: "Cancel",
   },
   confirming: {
     type: Boolean,
-    default: false
+    default: false,
   },
   loadingText: {
     type: String,
-    default: "Processing"
+    default: "Processing",
   },
   variant: {
     type: String,
     default: "danger", // or 'warning'
-    validator: (v) => ["danger","warning"].includes(v),
-  }
+    validator: (v) => ["danger", "warning", "success"].includes(v),
+  },
 });
 
 const emit = defineEmits(["confirm", "cancel"]);
 
 const onConfirm = () => emit("confirm");
-const onCancel = () => emit("cancle");
+const onCancel = () => emit("cancel");
 
 const styleClasses = computed(() => {
   let container = "";
@@ -71,11 +66,18 @@ const styleClasses = computed(() => {
   switch (props.variant) {
     case "danger":
       container = "bg-cc-17 border-cc-red text-cc-1";
-      confirm = "bg-cc-red text-white px-4.5 py-2.5 rounded hover:bg-cc-red-dark";
+      confirm =
+        "bg-cc-red text-white px-4.5 py-2.5 rounded hover:bg-cc-red-dark";
       break;
     case "warning":
       container = "bg-cc-17 border-cc-yellow text-cc-1";
-      confirm = "bg-cc-yellow text-black px-4.5 py-2.5 rounded hover:bg-cc-yellow-dark hover:text-white";
+      confirm =
+        "bg-cc-yellow text-black px-4.5 py-2.5 rounded hover:bg-cc-yellow-dark hover:text-white";
+      break;
+    case "success":
+      container = "bg-cc-17 border-cc-green text-cc-1";
+      confirm =
+        "bg-cc-green text-white px-4.5 py-2.5 rounded hover:bg-cc-green-dark hover:text-white";
       break;
   }
 
