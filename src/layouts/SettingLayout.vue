d3bc9cfb4a524a8cb026ff5a30d2a2273ebc6379
wey <qaz237562@gmail.com>
2025-06-25 10:07:51 +0800

    [refactor]調整設定頁面佈局，改善響應式設計與可讀性

--- src/layouts/SettingLayout.vue
+++ src/layouts/SettingLayout.vue
@@ -1,16 +1,18 @@
 <template>
-  <div class="content flex flex-row mt-3 border-t-3 border-t-[#2C303A]">
+  <div
+    class="content flex flex-col md:flex-row mt-3 border-t-3 border-t-[#2C303A]"
+  >
     <div
-      class="bg-[#131417] text-white pt-4 w-[180px] border-r-3 border-r-[#2C303A] h-full"
+      class="bg-[#131417] text-white pt-2 md:pt-4 w-full md:w-[180px] border-b-3 md:border-b-0 md:border-r-3 border-[#2C303A] md:h-full"
     >
-      <h2 class="text-xl font-bold mb-3 px-3">Settings</h2>
+      <h2 class="text-xl font-bold mb-3 px-3 hidden md:block">Settings</h2>
       <nav>
-        <ul class="text-gray-400 text-lg">
+        <ul class="flex flex-row md:flex-col text-gray-400 text-lg">
           <li
-            class="cursor-pointer p-1 pl-5 transition-all hover:bg-[#2C303A] hover:border-l-4 hover:border-[#05DF72] hover:text-white"
+            class="flex-1 md:flex-none cursor-pointer p-1 px-2 md:pl-5 transition-all hover:bg-[#2C303A] hover:border-l-4 hover:border-[#05DF72] hover:text-white"
             :class="
               route.path === '/settings/profile'
-                ? 'bg-[#2C303A] border-l-4 border-[#05DF72] text-white'
+                ? 'bg-[#2C303A] md:border-l-4 border-[#05DF72] text-white'
                 : ''
             "
             @click="profile"
@@ -18,10 +20,10 @@
             Profile
           </li>
           <li
-            class="cursor-pointer p-1 pl-5 transition-all hover:bg-[#2C303A] hover:border-l-4 hover:border-[#05DF72] hover:text-white"
+            class="flex-1 md:flex-none cursor-pointer p-1 px-2 md:pl-5 transition-all hover:bg-[#2C303A] hover:border-l-4 hover:border-[#05DF72] hover:text-white"
             :class="
               route.path === '/settings/account'
-                ? 'bg-[#2C303A] border-l-4 border-[#05DF72] text-white'
+                ? 'bg-[#2C303A] md:border-l-4 border-[#05DF72] text-white'
                 : ''
             "
             @click="account"
@@ -29,10 +31,10 @@
             Account
           </li>
           <li
-            class="cursor-pointer p-1 pl-5 transition-all hover:bg-[#2C303A] hover:border-l-4 hover:border-[#05DF72] hover:text-white"
+            class="flex-1 md:flex-none cursor-pointer p-1 px-2 md:pl-5 transition-all hover:bg-[#2C303A] hover:border-l-4 hover:border-[#05DF72] hover:text-white"
             :class="
               route.path === '/settings/billing'
-                ? 'bg-[#2C303A] border-l-4 border-[#05DF72] text-white'
+                ? 'bg-[#2C303A] md:border-l-4 border-[#05DF72] text-white'
                 : ''
             "
             @click="bill"
@@ -42,7 +44,9 @@
         </ul>
       </nav>
     </div>
-    <RouterView />
+    <div class="flex-1">
+      <RouterView />
+    </div>
   </div>
 </template>
 
@@ -57,7 +61,6 @@ const profile = () => {
 const account = () => {
   router.push("/settings/account");
 };
-
 const bill = () => {
   router.push("/settings/billing");
 };
