7d6e28b1dff9a78dab5748a68f7ba5dbeb59f07d
wey <qaz237562@gmail.com>
2025-06-25 10:27:51 +0800

    [refactor]調整登入頁面佈局，改善響應式設計與可讀性

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,34 +1,46 @@
 <template>
-  <div class="flex flex-col text-white font-sans bg pt-16">
-    <div class="mx-auto">
-      <div class="max-w-[960px] w-full pl-28 pr-0">
+  <div class="flex flex-col text-white font-sans bg pt-8 sm:pt-12 md:pt-16">
+    <div class="mx-auto w-full">
+      <div class="max-w-[960px] w-full px-4 sm:px-8 md:pl-28 md:pr-0 mx-auto">
         <div class="text-xl font-bold text-white tracking-wide select-none">
           Codecaine
         </div>
-        <div class="text-[4.5rem] font-extrabold select-none">Log In</div>
+        <div
+          class="text-3xl sm:text-5xl md:text-[4.5rem] font-extrabold select-none pb-4"
+        >
+          Log In
+        </div>
       </div>
 
-      <div class="flex max-w-[960px] w-full rounded-lg overflow-hidden px-8">
-        <div class="flex-1 pr-16 flex flex-col items-end">
+      <div
+        class="flex flex-col md:flex-row max-w-[960px] w-full rounded-lg overflow-hidden px-2 sm:px-4 md:px-8 mx-auto"
+      >
+        <div
+          class="w-full md:w-1/2 md:pr-16 flex flex-col items-end mb-8 md:mb-0"
+        >
           <button
             @click="() => socialSignIn(new GoogleAuthProvider())"
-            class="w-3/4 flex items-center py-3 px-4 mb-4 bg-[#444857] rounded cursor-pointer transition hover:bg-[black]">
+            class="w-full sm:w-3/4 flex items-center py-3 px-4 mb-4 bg-[#444857] rounded cursor-pointer transition hover:bg-[black]"
+          >
             <GoogleIcon class="w-5 h-5 mr-3" />
             Log In with Google
           </button>
           <button
             @click="() => socialSignIn(new GithubAuthProvider())"
-            class="w-3/4 flex items-center py-3 px-4 mb-4 bg-[#444857] rounded cursor-pointer transition hover:bg-[black]">
+            class="w-full sm:w-3/4 flex items-center py-3 px-4 mb-4 bg-[#444857] rounded cursor-pointer transition hover:bg-[black]"
+          >
             <GithubIcon class="w-5 h-5 mr-3" />
             Log In with GitHub
           </button>
 
           <div
-            class="w-3/4 rounded overflow-hidden transition"
-            :class="{ 'bg-[#323444]': infoOpen }">
+            class="w-full sm:w-3/4 rounded overflow-hidden transition"
+            :class="{ 'bg-[#323444]': infoOpen }"
+          >
             <div
               class="cursor-pointer font-medium text-base leading-tight px-4 py-2 select-none relative"
-              @click="toggleInfo">
+              @click="toggleInfo"
+            >
               <span
                 class="inline-block text-2xl mr-2 align-middle transition-transform"
                 :class="{ 'rotate-90': infoOpen }"
@@ -41,7 +53,8 @@
               :class="
                 infoOpen ? 'max-h-40 opacity-100 ' : 'max-h-0 opacity-0 py-0'
               "
-              ref="infoContent">
+              ref="infoContent"
+            >
               <p class="leading-tight text-sm">
                 If the email address associated with your social account matches
                 the email address of your CodePen account, you'll be logged in.
@@ -50,48 +63,45 @@
             </div>
           </div>
         </div>
-
-        <div class="relative flex items-center justify-center self-stretch w-8">
+        <div
+          class="relative items-center justify-center self-stretch w-full h-12 md:w-8 md:h-auto mb-4 md:mb-0 hidden md:flex"
+        >
           <div
-            class="absolute top-0 left-1/2 -translate-x-1/2 w-px h-[41%] bg-[white]"></div>
+            class="absolute top-0 left-1/2 -translate-x-1/2 w-px h-1/2 bg-white md:h-[41%]"
+          ></div>
           <span
-            class="absolute w-12 h-[18%] left-1/2 top-[41%] -translate-x-1/2 border-2 border-white text-center flex items-center justify-center rounded-[10px] text-lg leading-[30px] bg-[#1e1f26] text-gray"
+            class="absolute w-12 h-8 left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white text-center flex items-center justify-center rounded-[10px] text-lg leading-[30px] bg-[#1e1f26] text-gray"
             >OR</span
           >
           <div
-            class="absolute left-1/2 -translate-x-1/2 w-px h-[41%] bg-white top-[59%]"></div>
+            class="absolute left-1/2 -translate-x-1/2 w-px h-1/2 bg-white md:h-[41%] top-1/2 md:top-[59%]"
+          ></div>
         </div>
-
-        <div class="flex-1 flex flex-col px-16 pb-8">
+        <div class="flex-1 flex flex-col px-4 sm:px-8 md:px-16 pb-4">
           <form @submit.prevent="login">
             <div class="mb-5">
-              <label
-                for="username"
-                class="block mb-1 text-sm"
-                >Email</label
-              >
+              <label for="username" class="block mb-1 text-sm">Email</label>
               <input
                 v-model="email"
                 id="username"
                 type="text"
-                class="w-full py-3 px-4 rounded bg-[#b3b4ba] text-black border-none focus:bg-[white] focus:outline-none focus:ring-2 focus:ring-[#38c172] text-base" />
+                class="w-full py-3 px-4 rounded bg-[#b3b4ba] text-black border-none focus:bg-[white] focus:outline-none focus:ring-2 focus:ring-[#38c172] text-base"
+              />
             </div>
             <div class="mb-5">
-              <label
-                for="password"
-                class="block mb-1 text-sm"
-                >Password</label
-              >
+              <label for="password" class="block mb-1 text-sm">Password</label>
               <input
                 v-model="password"
                 id="password"
                 type="password"
                 autocomplete="off"
-                class="w-full py-3 px-4 rounded bg-[#b3b4ba] text-black border-none focus:bg-[white] focus:outline-none focus:ring-2 focus:ring-[#38c172] text-base" />
+                class="w-full py-3 px-4 rounded bg-[#b3b4ba] text-black border-none focus:bg-[white] focus:outline-none focus:ring-2 focus:ring-[#38c172] text-base"
+              />
             </div>
             <button
               type="submit"
-              class="w-full py-3 bg-[#38c172] rounded text-[#1e1f26] text-base font-medium cursor-pointer transition hover:bg-[#248C46] hover:text-white">
+              class="w-full py-3 bg-[#38c172] rounded text-[#1e1f26] text-base font-medium cursor-pointer transition hover:bg-[#248C46] hover:text-white"
+            >
               Log In
             </button>
           </form>
@@ -113,15 +123,14 @@
                 : 'max-h-0 opacity-0 p-0 mt-0'
             "
             class="overflow-hidden transition-all duration-500 ease-in-out bg-[#323444] rounded"
-            ref="resetBox">
+            ref="resetBox"
+          >
             <h2 class="mt-0 text-2xl font-semibold mb-4">
               Reset Your Password
             </h2>
             <form>
               <div class="mb-5">
-                <label
-                  for="reset-email"
-                  class="block mb-1 text-sm"
+                <label for="reset-email" class="block mb-1 text-sm"
                   >Email</label
                 >
                 <input
@@ -129,22 +138,20 @@
                   id="reset-email"
                   type="text"
                   placeholder="your@email.com"
-                  class="w-full py-3 px-4 rounded bg-[#b3b4ba] text-black border-none focus:bg-[white] focus:outline-none focus:ring-2 focus:ring-[#38c172] text-base" />
+                  class="w-full py-3 px-4 rounded bg-[#b3b4ba] text-black border-none focus:bg-[white] focus:outline-none focus:ring-2 focus:ring-[#38c172] text-base"
+                />
               </div>
               <button
                 type="button"
                 @click="handleResetPassword"
-                class="w-full py-3 bg-[#3a3c46] text-[#ececf1] rounded text-base cursor-pointer transition hover:bg-[#4b4e5a]">
+                class="w-full py-3 bg-[#3a3c46] text-[#ececf1] rounded text-base cursor-pointer transition hover:bg-[#4b4e5a]"
+              >
                 Send Password Reset Email
               </button>
-              <p
-                v-if="resetError"
-                class="text-red-400 mt-2">
+              <p v-if="resetError" class="text-red-400 mt-2">
                 {{ resetError }}
               </p>
-              <p
-                v-if="resetSuccess"
-                class="text-green-400 mt-2">
+              <p v-if="resetSuccess" class="text-green-400 mt-2">
                 {{ resetSuccess }}
               </p>
             </form>
@@ -153,7 +160,8 @@
       </div>
 
       <footer
-        class="max-w-[960px] w-full p-4 flex justify-center items-center mt-4">
+        class="max-w-[960px] w-full p-4 flex justify-center items-center mt-4 mx-auto"
+      >
         <div class="text-sm text-[#ececf1]">
           Need an account?
           <span
