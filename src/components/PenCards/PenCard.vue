d42b9e3ce2511ac4daf899db57207afaae5d6d69
wey <45823398+WeyTing@users.noreply.github.com>
2025-06-29 17:22:48 +0800

    [fix]修正 proLink 路徑

--- src/components/PenCards/PenCard.vue
+++ src/components/PenCards/PenCard.vue
@@ -8,15 +8,13 @@
           :src="iframeSrc"
           sandbox="allow-scripts"
           class="w-full h-full border-0"
-          loading="lazy"
-        ></iframe>
+          loading="lazy"></iframe>
       </div>
 
       <!-- 圖片右上角的方塊小連結 跳出 Modal -->
       <PenDetailsButton
         @open-detail-modal="openDetailModal"
-        class="absolute top-2 right-2 opacity-0 group-hover:opacity-100 transition"
-      />
+        class="absolute top-2 right-2 opacity-0 group-hover:opacity-100 transition" />
     </div>
 
     <!-- 卡片內容 -->
@@ -24,26 +22,25 @@
       <div class="flex items-center justify-between w-full">
         <div class="flex items-center gap-3 min-w-0">
           <!-- 左：頭像 -->
-          <a :href="userPageLink" class="shrink-0">
+          <a
+            :href="userPageLink"
+            class="shrink-0">
             <img
               :src="userProfileImage"
               class="w-10 h-10 rounded-sm object-cover"
-              :alt="userDisplayName + ' 的頭像'"
-            />
+              :alt="userDisplayName + ' 的頭像'" />
           </a>
           <!-- 中：標題與作者 -->
           <div class="flex-1 min-w-0 mr-2">
             <a
               :href="editorPageLink"
-              class="block font-bold text-base text-white w-full max-w-full overflow-hidden whitespace-nowrap truncate"
-            >
+              class="block font-bold text-base text-white w-full max-w-full overflow-hidden whitespace-nowrap truncate">
               {{ title }}
             </a>
             <div class="flex gap-2">
               <a
                 :href="userPageLink"
-                class="block text-sm text-gray-300 hover:underline truncate"
-              >
+                class="block text-sm text-gray-300 hover:underline truncate">
                 <span class="font-medium">{{
                   userDisplayName || userName
                 }}</span>
@@ -51,8 +48,7 @@
               <a
                 v-if="isPro"
                 :href="proLink"
-                class="bg-yellow-400 text-black text-[10px] font-bold px-1 py-[1px] rounded hover:bg-yellow-300 transition inline-flex items-center justify-center"
-              >
+                class="bg-yellow-400 text-black text-[10px] font-bold px-1 py-[1px] rounded hover:bg-yellow-300 transition inline-flex items-center justify-center">
                 PRO
               </a>
             </div>
@@ -71,8 +67,7 @@
             @follow="handleFollow"
             @togglePrivacy="togglePrivacy"
             @delete="handleDelete"
-            @toggle="$emit('toggle', pen.id)"
-          />
+            @toggle="$emit('toggle', pen.id)" />
         </div>
       </div>
 
@@ -82,9 +77,10 @@
         <PenCommentButton
           :work-id="workId"
           :comments="comments"
-          @openDetailModal="openDetailModal"
-        />
-        <PenViewButton :count="views" @goToFullPage="goToFullPage" />
+          @openDetailModal="openDetailModal" />
+        <PenViewButton
+          :count="views"
+          @goToFullPage="goToFullPage" />
       </div>
     </div>
   </div>
@@ -157,7 +153,7 @@ const views = props.pen.views_count;
 // 連結
 const editorPageLink = `/${userName}/dose/${workId}`;
 const userPageLink = `/${userName}`;
-const proLink = "/features/pro"; //目前還沒設定，先參考官方route暫定 /features/pro
+const proLink = "/settings/billing";
 
 const isOwner = computed(() => authStore.userProfile?.username === userName);
 
