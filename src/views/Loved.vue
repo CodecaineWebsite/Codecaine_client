05ac45345f15371061653c927c5f05f384350e7d
wey <45823398+WeyTing@users.noreply.github.com>
2025-06-29 17:13:35 +0800

    [fix]修復錯誤api路徑

--- src/views/Loved.vue
+++ src/views/Loved.vue
@@ -4,15 +4,15 @@
       v-if="!isLoading && pens.length > 0"
       :pens="pens"
       :mode="viewMode"
-      @pen-clicked="modalStore.openPenDetailModal($event)"
-    />
+      @pen-clicked="modalStore.openPenDetailModal($event)" />
     <div
       v-else-if="!isLoading && pens.length === 0"
-      class="justify-center flex mt-12"
-    >
+      class="justify-center flex mt-12">
       <p class="text-4xl bg-gray-800 p-12 rounded-lg">
         ⚠️ No Loved Doses found.
-        <a href="/pen" class="text-blue-200 hover:text-blue-400">
+        <a
+          href="/pen"
+          class="text-blue-200 hover:text-blue-400">
           Go create some first!
         </a>
       </p>
@@ -21,8 +21,7 @@
       :currentPage="page"
       :totalPages="totalPages"
       @prev="page--"
-      @next="page++"
-    />
+      @next="page++" />
   </div>
 </template>
 
@@ -48,15 +47,12 @@ const { showToast } = toastStore;
 const fetchCaines = async () => {
   isLoading.value = true;
   try {
-    const res = await api.get(
-      `/api/usersCaines/${route.params.username}/private`,
-      {
-        params: {
-          page: page.value,
-          view: viewMode.value,
-        },
-      }
-    );
+    const res = await api.get(`/api/favorites/${route.params.username}`, {
+      params: {
+        page: page.value,
+        view: viewMode.value,
+      },
+    });
     pens.value = res.data.results || [];
     totalPages.value = res.data.totalPages || 0;
   } catch (error) {
