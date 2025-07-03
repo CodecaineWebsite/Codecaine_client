d19ce0dc6a5a7f078f737376dfad734d5b13f8d0
Vivi <sun20317@gmail.com>
2025-07-03 10:00:39 +0800

    [feat] profile頁空資料畫面樣式更新

--- src/views/Showcase.vue
+++ src/views/Showcase.vue
@@ -18,15 +18,13 @@
       </button>
     </div>
     <div
-      class="justify-center flex m-12"
       v-else-if="!isLoading && pens.length === 0"
+      class="max-w-xl p-8 mb-5 mx-auto bg-cc-14 text-center rounded"
     >
-      <p class="text-4xl bg-gray-800 p-12 rounded-lg">
-        ⚠️ No Doses found.
-        <a href="/dose" class="text-blue-200 hover:text-blue-400">
-          Go create some first!
-        </a>
-      </p>
+      <h1 class="mb-2 leading-[1.1] font-archivo text-4xl">No Doses found.</h1>
+      <a href="/dose" class="text-blue-200 hover:text-blue-400">
+        Go create some first!
+      </a>
     </div>
   </div>
 </template>
